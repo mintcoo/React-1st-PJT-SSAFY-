c75448acd0f710ffd69017b4a59efdb904c528c4
HanSangHyun <xhaktm3344@gmail.com>
2023-01-28 23:59:59 +0900

    0128

--- front-client/src/App.tsx
+++ front-client/src/App.tsx
@@ -6,14 +6,16 @@ import SignUp from "./components/SignUp/Signup";
 import Login from "./components/Login/Login";
 import MainCreateRoom from "./components/Main/MainCreateRoom";
 import LayoutPage from "./components/Common/LayoutPage";
+import Loading from "./components/Common/Loading";
 function App() {
   return (
     <div className="App bg-black">
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route path="/" element={<Login />} />       
+        <Route path="/main" element={<Main />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<Login />} />       
-        <Route path="/create" element={<MainCreateRoom />} />
+        <Route path="/loading" element={<Loading />} />       
       </Routes>
     </div>
   );
